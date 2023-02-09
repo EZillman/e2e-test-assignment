997daa6b466a783fb7057bfc0d315f8080f2cf84
EZillman <evelina.zillman@medieinstitutet.se>
2023-02-09 19:57:41 +0100

    some basic tests

--- cypress/e2e/movies.cy.ts
+++ cypress/e2e/movies.cy.ts
@@ -2,7 +2,40 @@ beforeEach(() => {
   cy.visit('/');
 });
 
-describe('template spec', () => {
-  it('passes', () => {
-  })
-})
+describe('input field', () => {
+  it('should check for input', () => {
+    cy.get('input#searchText').should('exist');
+  });
+
+  it('should check if input works', () => {
+    cy.get('input#searchText').type('chocolate').should('have.value', 'chocolate');
+  });
+
+});
+
+describe('submit button', () => {
+  it('should check for submit button', () => {
+    cy.get('button#search').should('exist');
+  });
+
+});
+
+describe('searching for movies', () => {
+  it('should find movies', () => {
+    cy.get('input#searchText').type('chocolate').should('have.value', 'chocolate');
+
+    //cy.get('button#search').click();
+  });
+
+});
+
+describe('trying to submit empty input', () => {
+  it('should get error', () => {
+    cy.get('input#searchText').type(' ').should('have.value', ' ');
+
+    cy.get('button#search').click();
+
+    cy.get('p').contains('Inga sökresultat att visa').should('exist');
+  });
+
+});
